1d995b84f22c4edfe43860ef35ef14af5fcea8ee
Express Edition User <none@example.com>
2018-07-12 13:41:43 -0700

    Updated by Otter.

--- scripts/Test-PowerShellModuleInstalled.ps1
+++ scripts/Test-PowerShellModuleInstalled.ps1
@@ -1,4 +1,7 @@
-param([string]$ModuleName)
+param(
+    [parameter(Mandatory=$true,
+        HelpMessage="The name of the module ID in the PowerShell Gallery"
+    [string]$ModuleName)
 
 # Test for module
 if(Get-Module -Name $ModuleName -List -ErrorAction SilentlyContinue) { $true } else { $false }

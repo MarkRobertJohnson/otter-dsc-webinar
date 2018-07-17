ef5e44110be7e2923489c4f1a9c906fec56d2374
Express Edition User <none@example.com>
2018-07-17 01:33:43 -0700

    Updated by Otter.

---
+++ scripts/DSC_Win2016Server.ps1
@@ -0,0 +1,20 @@
+Configuration ServerConfig
+{ 
+    Registry RegistryExample
+    {
+        Ensure      = "Present"  # You can also set Ensure to "Absent"
+        Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\ExampleKey2"
+        ValueName   = "TestValue"
+        ValueData   = "TestData"
+    }
+    
+    foreach($path in @('my/path1','my/path2','my/path3')) {
+        Environment SetVarExample
+        {
+            Ensure      = "Present"
+            Name        = "MYCUSTOMPATH"
+            Value       = $path
+            Path        = $true
+        }
+    }
+}

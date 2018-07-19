79a0f960e76f08db4b0f948d093d3ce2ee20e7ba
Mark Johnson <mjohnson@axian.com>
2018-07-19 09:52:33 -0700

    Initial Commit

---
+++ scripts/DSC_Win2016Server.ps1
@@ -0,0 +1,22 @@
+Configuration ServerConfig
+{ 
+    Import-DscResource –ModuleName PSDscResources
+    
+    Registry RegistryExample
+    {
+        Ensure      = "Present"  # You can also set Ensure to "Absent"
+        Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\ExampleKey2"
+        ValueName   = "TestValue"
+        ValueData   = "TestData"
+    }
+    
+    foreach($var in (@{'var1'='my/path4';'var2' = 'my/path5'; 'var3' = 'my/path6'}).GetEnumerator()) {
+        Environment $var.name
+        {
+            Ensure      = "Present"
+            Name        = $var.name
+            Value       = $var.value
+            Target      = 'Machine'
+        }
+    }
+}
